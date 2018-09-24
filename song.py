e5238a5fc023c72981cb598dedae45c1818aa2cf
Eric <eric.w.yuan@outlook.com>
2018-09-24 00:41:52 -0400

    Added functions and documentation for Song

---
+++ song.py
@@ -0,0 +1,48 @@
+import pafy
+
+class Song:
+    def __init__(self, url, vlc):
+        """
+        Instantiates a Song object with a url and vlc object.
+
+        :param url: str
+        :param vlc: str
+        :return: None
+        """
+        self.__url = url
+        self.__vlc = vlc
+        self.__audio = pafy.new(url)
+        self.__media = self.__vlc.media_new(self.get_audio_url())
+
+    def get_title(self):
+        """
+        Returns the title of this Song.
+
+        :return: str
+        """
+        return self.__audio.title
+
+    def get_length(self):
+        """
+        Returns the length of this Song.
+
+        :return: str
+        """
+        return self.__audio.length
+
+    def get_audio_url(self):
+        """
+        Returns the audio URL of this Song.
+
+        :return: str
+        """
+        return self.__audio.url
+
+    def get_media(self):
+        """
+        Returns the media of this Song.
+
+        :return: str
+        """
+        return self.__media
+
