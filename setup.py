0e4c3fc3cac2bbaac8d9232abb2f6a3d91cc1b99
Eric Yuan <ewyuan@users.noreply.github.com>
2018-09-25 03:03:09 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup
 
 setup(
-    name='songify',
-    packages=['songify'],
+    name='py-mp',
+    packages=['py-mp'],
     author='EddyMaric, ewyuan',
     author_email='eddy.maric98@gmail.com, eric.yuan@mail.utoronto.ca',
     description='A simple command-line music player',
-    long_description='Songify uses command-line to take in inputs from the user and makes a search query on Youtube. Songify will pull the most relevant video and will stream the audio to the computer.',
+    long_description='py-mp, short for python music player, uses command-line to take in inputs from the user and makes a search query on Youtube. py-mp will pull the most relevant video and will stream the audio to the computer.',
     license='MIT',
-    url='http://github.com/ewyuan/songify',
+    url='http://github.com/ewyuan/py-mp',
     entry_points={
         'console_scripts': [
-            'songify = songify.__main__:main'
+            'py-mp = py-mp.__main__:main'
         ]
     },
     version='1.0',
