d5fa5e1cbf53f9fbc62525bf4c75ca1c6db7f462
Eric <eric.w.yuan@outlook.com>
2018-09-25 10:49:21 -0400

    Updated version

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(
             'py-mp = pymp.__main__:main'
         ]
     },
-    version='1.0',
+    version='1.1',
     install_requires=['python-vlc', 'requests', 'beautifulsoup4', 'pafy', 'youtube-dl'],
 )
