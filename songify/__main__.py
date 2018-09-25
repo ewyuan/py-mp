89ed16042d3c64db0f477a2d43404a6ba4020072
EddyMaric <eddy.maric98@gmail.com>
2018-09-24 23:07:42 -0700

    Fixed infinite printing when song finishes.

--- songify/__main__.py
+++ songify/__main__.py
@@ -8,7 +8,7 @@ def main():
     input_thread = threading.Thread(target=handle_inputs, args=(player,))
     input_thread.start()
     while True:
-        if player.get_state().value == 6:
+        if player.get_state().value == 6 and player.get_queue_size() > 0:
             player.play_next()
         if threading.active_count() != 2:
             break
