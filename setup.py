783d2a0a5543750d48819430ec12a08013b6d541
Eric <eric.w.yuan@outlook.com>
2018-09-25 02:06:55 -0400

    Added long desciption and license in setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ setup(
     author='EddyMaric, ewyuan',
     author_email='eddy.maric98@gmail.com, eric.yuan@mail.utoronto.ca',
     description='A simple command-line music player',
+    long_description='Songify uses command-line to take in inputs from the user and makes a search query on Youtube. Songify will pull the most relevant video and will stream the audio to the computer.',
+    license='MIT',
     url='http://github.com/ewyuan/songify',
     entry_points={
         'console_scripts': [
