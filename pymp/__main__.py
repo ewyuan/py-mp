97b249eea9f42938bc39bf4cbd6545a061750a71
Eric <eric.w.yuan@outlook.com>
2018-09-26 14:45:45 -0400

    Added vlc

--- pymp/__main__.py
+++ pymp/__main__.py
@@ -1,5 +1,5 @@
 import threading
-import vlc
+from .vlc import *
 from .player import Player
 from .helper import handle_inputs
 
@@ -9,7 +9,7 @@ def main():
     input_thread = threading.Thread(target=handle_inputs, args=(player,))
     input_thread.start()
     while True:
-        if player.get_state().value == vlc.State.Ended and player.get_queue_size() > 0:
+        if player.get_state().value == State.Ended and player.get_queue_size() > 0:
             player.play_next()
         if threading.active_count() != 2:
             break
